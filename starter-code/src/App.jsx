a6838bb019510f8e81185d84df63817a526e3309
Miguel Carrilho <mangelott@gmail.com>
2019-09-24 12:27:00 +0100

    LIS-FTWD-MiguelCarrilho-156

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -5,27 +5,39 @@ import Tittle from "./components/Tittle";
 import "./App.css";
 import contacts from "./contacts.json";
 import Container from "react-bootstrap/Container";
-import ButtonToolbar from "react-bootstrap/ButtonToolbar";
+import Button from "react-bootstrap/ButtonToolbar";
 import ContactList from "./components/ContactList";
 
 class App extends Component {
   constructor() {
     super();
-    const contactList = contacts.slice(0, 5);
-    this.state = { contactList };
+    this.contactList = contacts.slice(0, 5);
+    this.state = { contactList: this.contactList };
     this.addNew = this.addNew.bind(this);
+    this.sort = this.sort.bind(this);
   }
 
   addNew() {
-    console.log("Adding new");
+    const randomContact = contacts[Math.floor(Math.random() * contacts.length)];
+    this.setState({
+      contactList: [...this.state.contactList, randomContact]
+    });
   }
+
+  sort() {
+    this.setState({
+      contactList: this.contactList.sort((a, b) => (a.name > b.name ? 1 : -1))
+    });
+  }
+
   render() {
     const contactList = this.state.contactList;
     // const message = "This is my app";
     return (
       <Container>
         <Tittle />
-        <ButtonToolbar onClick={this.addNew}>Add New Contact</ButtonToolbar>
+        <Button onClick={this.addNew}>Add New Contact</Button>
+        <Button onClick={this.sort}>SortContact</Button>
         <ContactList contacts={contactList} />
       </Container>
     );
--- starter-code/src/components/ContactItem.jsx
+++ starter-code/src/components/ContactItem.jsx
@@ -5,7 +5,12 @@ const ContactItem = ({ contact }) => {
   return (
     <tr>
       <td>
-        <img src={contact.pictureUrl} alt={contact.name} />
+        <img
+          src={contact.pictureUrl}
+          alt={contact.name}
+          width="100"
+          height="150"
+        />
       </td>
       <td>{contact.name}</td>
       <td>{contact.popularity}</td>
